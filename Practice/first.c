c309c7080afffefe987601cd371f138d64c2869d
Ankit Yadav <165555453+ankitydve13@users.noreply.github.com>
2024-03-31 15:58:30 +0530

    Add files via upload

---
+++ Practice/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("naruto entar two numbar");
+    scanf("%d %d", &a, &b);
+    printf("sum of %d and %d s %d", a, b, a + b);
+    printf("\n");
+
+    return 0;
+}
