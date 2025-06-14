44af0d6f8a6715e781dc7caed9c45babe9eb22fb
Ankit Yadav <165555453+ankitydve13@users.noreply.github.com>
2025-06-15 01:17:43 +0530

    Add files via upload

---
+++ Classwork/helloworld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    printf("great= hello world");
+    printf("\n");
+    printf("name =ankit yadav");
+    printf("\n");
+    printf("age =19 ");
+    printf("\n");
+    printf("6/8/2025");
+    printf("\n");
+    return 0;
+}
