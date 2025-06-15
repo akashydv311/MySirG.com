5f1d7c83c8818bfc0ddb9cfe5ba679adc1e45234
ankityadav <ankitydve13@gmail.com>
2025-06-15 22:56:46 +0530

    added question01 answer

---
+++ Questions/printf/question01.c
@@ -0,0 +1,14 @@
+
+/*
+Print your name and age using printf.
+Example Output:
+My name is Ankit and I am 20 years old.
+*/
+
+#include<stdio.h>
+int main(){
+
+    printf("My name is Ankit and I am 20 years old.");
+
+    return 0;
+}
